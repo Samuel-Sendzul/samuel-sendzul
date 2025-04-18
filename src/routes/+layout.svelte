9ffd6764787429dc78aa8b1becc404fcb80b4afd
Samuel Sendzul <samuel.sendzul@gmail.com>
2025-04-18 14:18:20 +0200

    feat: add home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,8 @@
 	let { children } = $props();
 </script>
 
+<nav class="bg-neutral-2 flex items-center justify-between p-4 md:p-8">
+	<a href="/" class="text-2xl font-semibold">samuelsendzul</a>
+</nav>
+
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center md:my-20 md:flex-col">
+	<div class="flex flex-col items-center gap-8 p-4 md:flex-row md:gap-8">
+		<div class="flex flex-col gap-2">
+			<span class="text-2xl font-bold text-neutral-500 md:text-4xl">Hi, My name is Sam!</span>
+			<img src="/images/DSC06260.jpg" alt="Sam" class="w-80 md:w-88" />
+		</div>
+		<div class="flex flex-col gap-4 px-8 text-xl md:w-90 md:text-2xl">
+			<span class="font-semibold">Welcome to my website.</span>
+			<span>I'm a full stack software engineer and entrepreneur.</span>
+			<span
+				>Here, you'll find a collection of my coding projects, writing about the businesses I’ve
+				started, and some personal reflections.</span
+			>
+			<span>I hope you enjoy and feel free to get in touch!</span>
+		</div>
+	</div>
+</div>
